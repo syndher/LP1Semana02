700d4ab1a1a04b8c6a51d7ed3187d76290c4ed5c
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-27 21:37:58 +0000

    Added an exception for turn 10

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace BreakContinue
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                // Ends loop if we're over the maximum number of turns
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+
+                // Skips current cycle if current turn is divisible by 3
+                if (turn % 3 == 0 || turn == 10) 
+                {
+                    continue;
+                }
+
+                // Print turn number
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            // Print number of last turn
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}
