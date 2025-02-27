3085d7982c66c3c2ff57207fecf95da600d7dcaa
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-27 19:55:39 +0000

    Added SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿using System ;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+    namespace SmoothOperator
+    {
+      
+        class Program
+        {
+           
+        static void Main (string[] args)
+            {
+                Console.WriteLine("Insere número inteiro:");
+                string inteiroStr = Console.ReadLine();
+                sbyte inteiro = sbyte.Parse(inteiroStr);
+                inteiro = --inteiro;
+                Console.WriteLine(inteiro);
+                inteiro = ++inteiro;
+                Console.WriteLine(inteiro);
+
+
+            }
+        }
+    }
