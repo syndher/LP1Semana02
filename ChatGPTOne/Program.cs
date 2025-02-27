827157f779090131abc6cf5bbf97cc9e9e2cc3b7
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-27 22:05:11 +0000

    added 4 questions

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+  
+            // Variables
+            string question;
+            string response;
+
+            // Asks the user for a question
+            Console.Write("Ask me something: ");
+            question = Console.ReadLine();
+
+                    switch (question)
+                {
+                    case "Who are you?":
+                        response = "ChatGPTOne";
+                        break;
+                    case "What are you?":
+                        response = "I'm a script";
+                        break;
+                    case "What is your purpose?":
+                        response = "Answer to questions";
+                        break;
+                    case "Are you alive":
+                        response = "I wish";
+                        break;
+                    case "EXIT":
+                        response = "Bye";
+                        break;
+                    default:
+                        response = "I can't answer that";
+                        break;
+               }
+
+                
+                Console.WriteLine(response);
+        }   
+    }
+}
