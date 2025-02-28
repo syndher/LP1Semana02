865717e68eb2f693c8a906ee0be2e4eda72c4a60
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-28 17:17:17 +0000

    Added if statements

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System ;
+
+
+    namespace GuessTheNumber
+    {
+      
+        class Program
+        {
+           
+        static void Main (string[] args)
+            {
+                Random rnd = new Random();
+                int numberToGuess = rnd.Next(31);
+                Console.WriteLine("Choose a number between 0 and 30");
+                string nmb = Console.ReadLine();
+                int nmbint = int.Parse(nmb);
+                if (nmbint > -1 && nmbint < 31);
+                
+
+                else
+                    Console.WriteLine("That number is invalid, pick another");
+                    string nmb2 = Console.ReadLine();
+                    int nmbint2 = int.Parse(nmb2);
+                    nmbint = nmbint2;
+
+
+                    
+            {
+
+               }
+
+                if (question != "EXIT")
+                Console.WriteLine(response);
+                Console.Write("Ask me something: ");
+                question = Console.ReadLine();
+
+
+            }
+        }
+    }
